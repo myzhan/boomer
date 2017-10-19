6cd3546f734099aba2843f1f46da63cd1182b5fe
myzhan <scau.myzhan@gmail.com>
2017-10-19 17:38:24 +0800

    add http_benchmark example

---
+++ examples/http_benchmark.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"bytes"
+	"crypto/tls"
+	"flag"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/myzhan/boomer"
+)
+
+// This is a tool like Apache Benchmark a.k.a "ab".
+// It doesn't implement all the features supported by ab.
+
+var client *http.Client
+var postBody []byte
+
+var verbose bool
+
+var method string
+var url string
+var timeout int
+var postFile string
+var contentType string
+
+var disableCompression bool
+var disableKeepalive bool
+
+func worker() {
+
+	request, err := http.NewRequest(method, url, bytes.NewBuffer(postBody))
+	if err != nil {
+		log.Fatalf("%v\n", err)
+	}
+
+	request.Header.Set("Content-Type", contentType)
+
+	startTime := boomer.Now()
+
+	response, err := client.Do(request)
+
+	endTime := boomer.Now()
+
+	if err != nil {
+		if verbose {
+			log.Printf("%v\n", err)
+		}
+		boomer.Events.Publish("request_failure", "http", "error", 0.0, err.Error())
+	} else {
+		if response.StatusCode == http.StatusOK {
+			boomer.Events.Publish("request_success", "http", "200",
+				float64(endTime-startTime), response.ContentLength)
+		} else {
+			boomer.Events.Publish("request_success", "http", string(response.StatusCode),
+				float64(endTime-startTime), response.ContentLength)
+		}
+
+		if verbose {
+			body, err := ioutil.ReadAll(response.Body)
+			if err != nil {
+				log.Printf("%v\n", err)
+			} else {
+				log.Printf("Status Code: %d\n", response.StatusCode)
+				log.Println(string(body))
+			}
+
+		} else {
+			io.Copy(ioutil.Discard, response.Body)
+		}
+
+		response.Body.Close()
+	}
+
+}
+
+func main() {
+
+	flag.StringVar(&method, "method", "GET", "HTTP method, one of GET, POST")
+	flag.StringVar(&url, "url", "", "URL")
+	flag.IntVar(&timeout, "timeout", 10, "Seconds to max. wait for each response")
+	flag.StringVar(&postFile, "post-file", "", "File containing data to POST. Remember also to set --content-type")
+	flag.StringVar(&contentType, "content-type", "text/plain", "Content-type header")
+
+	flag.BoolVar(&disableCompression, "disable-compression", false, "Disable compression")
+	flag.BoolVar(&disableKeepalive, "disable-keepalive", false, "Disable keepalive")
+
+	flag.BoolVar(&verbose, "verbose", false, "Print debug log")
+
+	flag.Parse()
+
+	log.Printf(`HTTP benchmark is running with these args:
+method: %s
+url: %s
+timeout: %d
+post-file: %s
+content-type: %s
+disable-compression: %t
+disable-keepalive: %t
+verbose: %t`, method, url, timeout, postFile, contentType, disableCompression, disableKeepalive, verbose)
+
+	if url == "" {
+		log.Fatalln("--url can't be empty string, please specify a URL that you want to test.")
+	}
+
+	if method != "GET" && method != "POST" {
+		log.Fatalln("HTTP method must be one of GET, POST.")
+	}
+
+	if method == "POST" {
+		if postFile == "" {
+			log.Fatalln("--post-file can't be empty string when method is POST")
+		}
+		tmp, err := ioutil.ReadFile(postFile)
+		if err != nil {
+			log.Fatalf("%v\n", err)
+		}
+		postBody = tmp
+	}
+
+	http.DefaultTransport.(*http.Transport).MaxIdleConnsPerHost = 2000
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+		MaxIdleConnsPerHost: 2000,
+		DisableCompression:  disableCompression,
+		DisableKeepAlives:   disableKeepalive,
+	}
+	client = &http.Client{
+		Transport: tr,
+		Timeout:   time.Duration(timeout) * time.Second,
+	}
+
+	task := &boomer.Task{
+		Name:   "worker",
+		Weight: 10,
+		Fn:     worker,
+	}
+
+	boomer.Run(task)
+
+}
