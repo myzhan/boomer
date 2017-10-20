9a74df01baaa5444e538a47bdcb6084c6daee24a
myzhan <scau.myzhan@gmail.com>
2017-10-20 10:21:09 +0800

    FIX: building issue of client_czmq.go

--- client_czmq.go
+++ client_czmq.go
@@ -20,10 +20,10 @@ func newClient() client {
 	var client client
 	var message string
 	if *rpc == "zeromq" {
-		client := newZmqClient(*masterHost, *masterPort)
+		client = newZmqClient(*masterHost, *masterPort)
 		message = fmt.Sprintf("Boomer is connected to master(%s:%d|%d) press Ctrl+c to quit.", *masterHost, *masterPort, *masterPort+1)
 	} else if *rpc == "socket" {
-		client := newSocketClient(*masterHost, *masterPort)
+		client = newSocketClient(*masterHost, *masterPort)
 		message = fmt.Sprintf("Boomer is connected to master(%s:%d) press Ctrl+c to quit.", *masterHost, *masterPort)
 	} else {
 		log.Fatal("Unknown rpc type:", *rpc)
--- client_gomq.go
+++ client_gomq.go
@@ -1,3 +1,5 @@
+// +build !goczmq
+
 package boomer
 
 import (
