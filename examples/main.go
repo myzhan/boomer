3f1c8700720799ed84713d59b901c12f7a95efcb
myzhan <scau.myzhan@gmail.com>
2018-06-08 11:36:08 +0800

    FIX: comment

--- examples/main.go
+++ examples/main.go
@@ -9,10 +9,8 @@ func foo() {
 	time.Sleep(100 * time.Millisecond)
 	elapsed := boomer.Now() - start
 
-	/*
-		Report your test result as a success, if you write it in python, it will looks like this
-		events.request_success.fire(request_type="http", name="foo", response_time=100, response_length=10)
-	*/
+	// Report your test result as a success, if you write it in python, it will looks like this
+	// events.request_success.fire(request_type="http", name="foo", response_time=100, response_length=10)
 	boomer.Events.Publish("request_success", "http", "foo", elapsed, int64(10))
 }
 
@@ -22,10 +20,8 @@ func bar() {
 	time.Sleep(100 * time.Millisecond)
 	elapsed := boomer.Now() - start
 
-	/*
-		Report your test result as a failure, if you write it in python, it will looks like this
-		events.request_failure.fire(request_type="udp", name="bar", response_time=100, exception=Exception("udp error"))
-	*/
+	// Report your test result as a failure, if you write it in python, it will looks like this
+	// events.request_failure.fire(request_type="udp", name="bar", response_time=100, exception=Exception("udp error"))
 	boomer.Events.Publish("request_failure", "udp", "bar", elapsed, "udp error")
 }
 
