101bf511c50b757c861c685e3549db0a538d7f2d
myzhan <scau.myzhan@gmail.com>
2018-06-21 14:01:25 +0800

    add: pacing rps example

---
+++ examples/pacing_rps.go
@@ -0,0 +1,56 @@
+package main
+
+import "github.com/myzhan/boomer"
+import (
+	"sync/atomic"
+	"time"
+	"log"
+)
+
+var step = int64(1)
+var currentRPS = int64(0)
+var currentRPSThreshold = int64(0)
+var waitChannel = make(chan bool)
+
+func coworker() {
+
+	token := atomic.AddInt64(&currentRPSThreshold, -1)
+	if token < 0 {
+		<-waitChannel
+		return
+	}
+
+	boomer.Events.Publish("request_success", "rps", "pacing", int64(1), int64(10))
+
+}
+
+func updateThreshold() {
+
+	var ticker = time.NewTicker(1 * time.Second)
+	for {
+		select {
+		case <-ticker.C:
+			currentRPS = currentRPS + step
+			atomic.StoreInt64(&currentRPSThreshold, currentRPS)
+			log.Println("Current RPS", currentRPS)
+
+			close(waitChannel)
+			waitChannel = make(chan bool)
+		}
+	}
+
+}
+
+func main() {
+
+	task := &boomer.Task{
+		Name:   "foo",
+		Weight: 10,
+		Fn:     coworker,
+	}
+
+	go updateThreshold()
+
+	boomer.Run(task)
+
+}
