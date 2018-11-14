44f001e4e801f5ed60152dfb0d3458f668caf9e1
myzhan <scau.myzhan@gmail.com>
2018-11-14 10:55:39 +0800

    add test for events

--- events_test.go
+++ events_test.go
@@ -1,6 +1,8 @@
 package boomer
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestConvertResponseTime(t *testing.T) {
 	convertedFloat := convertResponseTime(float64(1.234))
@@ -18,4 +20,32 @@ func TestConvertResponseTime(t *testing.T) {
 	}()
 	// It should panic
 	convertResponseTime(1)
+}
+
+func TestInitEvents(t *testing.T) {
+	initEvents()
+	Events.Publish("request_success", "http", "foo", int64(1), int64(10))
+	Events.Publish("request_failure", "udp", "bar", int64(2), "udp error")
+
+	requestSuccessMsg := <-requestSuccessChannel
+	if requestSuccessMsg.requestType != "http" {
+		t.Error("Expected: http, got:", requestSuccessMsg.requestType)
+	}
+	if requestSuccessMsg.responseTime != int64(1) {
+		t.Error("Expected: 1, got:", requestSuccessMsg.responseTime)
+	}
+
+	requestFailureMsg := <-requestFailureChannel
+	if requestFailureMsg.requestType != "udp" {
+		t.Error("Expected: udp, got:", requestFailureMsg.requestType)
+	}
+	if requestFailureMsg.responseTime != int64(2) {
+		t.Error("Expected: 2, got:", requestFailureMsg.responseTime)
+	}
+	if requestFailureMsg.error != "udp error" {
+		t.Error("Expected: udp error, got:", requestFailureMsg.error)
+	}
+
+	Events.Unsubscribe("request_success", requestSuccessHandler)
+	Events.Unsubscribe("request_failure", requestFailureHandler)
 }
