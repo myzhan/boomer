cede808c4a3a36868c54466ef4c572afcfe7b2d0
myzhan <scau.myzhan@gmail.com>
2019-05-30 15:00:59 +0800

    FIX: specify master doc to work on readthedocs.org

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,6 +38,9 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# The master toctree document.
+master_doc = 'index'
+
 
 # -- Options for HTML output -------------------------------------------------
 
