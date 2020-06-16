cbe3e9cb716de15c477449452842fb047d7c6735
dechengyuan(袁德宬) <dechengyuan@tencent.com>
2020-06-16 16:33:29 +0800

    <add>:<add fasthttp>

--- examples/main.go
+++ examples/main.go
@@ -1,7 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"io/ioutil"
+
+	//"io"
+	//"io/ioutil"
+
 	"log"
+	"net"
+	"net/http"
 	"os"
 	"os/signal"
 	"sync"
@@ -9,17 +18,134 @@ import (
 	"time"
 
 	"github.com/myzhan/boomer"
+	"github.com/valyala/fasthttp"
 )
+
+func createHttpClient() *http.Client{
+	client := &http.Client{
+		Transport : &http.Transport{
+			Proxy: http.ProxyFromEnvironment,
+			DialContext: (&net.Dialer{
+				Timeout:   30 * time.Second,
+				KeepAlive: 30 * time.Second,
+				DualStack: true,
+			}).DialContext,
+			DisableKeepAlives: false,
+			ForceAttemptHTTP2:     true,
+			MaxIdleConns:          10000,
+			MaxIdleConnsPerHost:   10000,
+			IdleConnTimeout:       90 * time.Second,
+			TLSHandshakeTimeout:   10 * time.Second,
+			ExpectContinueTimeout: 1 * time.Second,
+		},
+	}
+	return client
+}
+
+var client *http.Client = createHttpClient()
 
 func foo() {
 	start := time.Now()
-	time.Sleep(100 * time.Millisecond)
+
+	var url string = `http://localhost:8080/hello`
+	resp, err := client.Get(url)
+	if err != nil {
+		//log.Println(err)
+		return
+	}
+
+	//all Write calls succeed without doing anything.
+	io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close()
+
+	//fmt.Println(resp.Status)
 	elapsed := time.Since(start)
 
 	// Report your test result as a success, if you write it in python, it will looks like this
 	// events.request_success.fire(request_type="http", name="foo", response_time=100, response_length=10)
-	globalBoomer.RecordSuccess("http", "foo", elapsed.Nanoseconds()/int64(time.Millisecond), int64(10))
+	if resp.StatusCode < 400  {
+		globalBoomer.RecordSuccess("http", "foo", elapsed.Nanoseconds()/int64(time.Millisecond), int64(10))
+	} else {
+		globalBoomer.RecordFailure("http", "foo", elapsed.Nanoseconds()/int64(time.Millisecond), resp.Status)
+	}
+
 }
+
+//use request and response pool
+func foo1(){
+	start := time.Now()
+
+	var req = fasthttp.AcquireRequest()
+	var resp = fasthttp.AcquireResponse()
+	var url string = `http://localhost:8080/hello`
+	req.Header.SetMethod("GET")
+	req.SetRequestURI(url)
+	defer func(){
+		fasthttp.ReleaseResponse(resp)
+		fasthttp.ReleaseRequest(req)
+	}()
+	//client := createHttpClient()
+
+	//statusCode, body, err := client.Get(body,"http://localhost:8080/hello")
+	//resp, err := client.Get("http://localhost:80/hello")
+
+	if err := fasthttp.Do(req, resp);err != nil {
+		//log.Println(err)
+		return
+	}
+
+	//resp.ReleaseBody(0)
+	//all Write calls succeed without doing anything.
+	//io.Copy(ioutil.Discard, resp.body)
+	//resp.body.Close()
+
+	//fmt.Println(resp.Status)
+	elapsed := time.Since(start)
+
+	// Report your test result as a success, if you write it in python, it will looks like this
+	// events.request_success.fire(request_type="http", name="foo", response_time=100, response_length=10)
+	if resp.StatusCode() < 400  {
+		globalBoomer.RecordSuccess("http", "foo", elapsed.Nanoseconds()/int64(time.Millisecond), int64(10))
+	} else {
+		globalBoomer.RecordFailure("http", "foo", elapsed.Nanoseconds()/int64(time.Millisecond), fmt.Sprint(resp.StatusCode()))
+	}
+}
+
+//use fasthttp.Client
+var fast_client *fasthttp.Client = &fasthttp.Client{
+	MaxConnsPerHost: 10000,
+}
+
+func foo2(){
+	start := time.Now()
+
+
+	var url string = `http://localhost:8080/hello`
+
+
+	statusCode, _, err := fast_client.Get(nil,url)
+	if err != nil {
+		//log.Println(err)
+		return
+	}
+
+	//resp.ReleaseBody(0)
+	//all Write calls succeed without doing anything.
+	//io.Copy(ioutil.Discard, resp.body)
+	//resp.body.Close()
+
+	//fmt.Println(resp.Status)
+	elapsed := time.Since(start)
+
+	// Report your test result as a success, if you write it in python, it will looks like this
+	// events.request_success.fire(request_type="http", name="foo", response_time=100, response_length=10)
+	if statusCode < 400  {
+		globalBoomer.RecordSuccess("http", "foo", elapsed.Nanoseconds()/int64(time.Millisecond), int64(10))
+	} else {
+		globalBoomer.RecordFailure("http", "foo", elapsed.Nanoseconds()/int64(time.Millisecond), fmt.Sprint(statusCode))
+	}
+}
+
 
 func bar() {
 	start := time.Now()
@@ -61,18 +187,25 @@ func main() {
 
 	task1 := &boomer.Task{
 		Name:   "foo",
+		// The weight is used to distribute goroutines over multiple tasks.
 		Weight: 10,
-		Fn:     foo,
+		Fn:     foo2,
 	}
 
-	task2 := &boomer.Task{
-		Name:   "bar",
-		Weight: 30,
-		Fn:     bar,
-	}
+	//task2 := &boomer.Task{
+	//	Name:   "bar",
+	//	Weight: 30,
+	//	Fn:     bar,
+	//}
 
-	globalBoomer.Run(task1, task2)
+	//globalBoomer.Run(task1, task2)
+	globalBoomer.Run(task1)
 
 	waitForQuit()
 	log.Println("shut down")
+
 }
+
+
+
+
