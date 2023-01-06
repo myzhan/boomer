85578965f557d4770dc7d8f76f0bc858b5b462e1
Dipjyoti Metia <dipjyotimetia@gmail.com>
2023-01-06 15:58:45 +1100

    adding support for exporting metrics to influxdb

--- output.go
+++ output.go
@@ -1,6 +1,7 @@
 package boomer
 
 import (
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -9,6 +10,9 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/google/uuid"
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/influxdata/influxdb-client-go/v2/api"
 	"github.com/olekukonko/tablewriter"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/push"
@@ -406,5 +410,71 @@ func (o *PrometheusPusherOutput) OnEvent(data map[string]interface{}) {
 
 	if err := o.pusher.Push(); err != nil {
 		log.Println(fmt.Sprintf("Could not push to Pushgateway: error: %v", err))
+	}
+}
+
+// InfluxOutput pushes boomer stats to InfluxDB.
+type InfluxOutput struct {
+	influx api.WriteAPI
+}
+
+// NewInfluxOutput returns a InfluxOutput.
+func NewInfluxOutput(influxHost, token, org, bucket string) *InfluxOutput {
+	return &InfluxOutput{
+		influxdb2.NewClientWithOptions(influxHost, token,
+			influxdb2.DefaultOptions().
+				SetUseGZip(true).
+				SetTLSConfig(&tls.Config{InsecureSkipVerify: true})).WriteAPI(org, bucket),
+	}
+}
+
+// OnStart will start influxdb write api
+func (o *InfluxOutput) OnStart() {
+	log.Println("register influx metric collectors")
+}
+
+// OnStop of InfluxOutput force all unwritten data to be sent
+func (o *InfluxOutput) OnStop() {
+	o.influx.Flush()
+}
+
+func (o *InfluxOutput) OnEvent(data map[string]interface{}) {
+	eventTime := time.Now()
+	errorsCh := o.influx.Errors()
+	go func() {
+		for err := range errorsCh {
+			fmt.Printf("could not push to influxdb error: %s\n\n", err.Error())
+		}
+	}()
+	output, err := convertData(data)
+	if err != nil {
+		log.Println(fmt.Sprintf("convert data error: %v", err))
+		return
+	}
+
+	for _, stat := range output.Stats {
+		method := stat.Method
+		name := stat.Name
+		point := influxdb2.NewPoint(
+			method+name,
+			map[string]string{
+				// unique run_id
+				"run_id":     uuid.New().String(),
+				"user_count": string(output.UserCount),
+				"total_rps":  strconv.FormatInt(output.TotalRPS, 10),
+			},
+			map[string]interface{}{
+				"num_requests":         float64(stat.NumRequests),
+				"num_failures":         float64(stat.NumFailures),
+				"median_response_time": float64(stat.medianResponseTime),
+				"avg_response_time":    stat.avgResponseTime,
+				"min_response_time":    float64(stat.MinResponseTime),
+				"max_response_time":    float64(stat.MaxResponseTime),
+				"avg_content_length":   float64(stat.avgContentLength),
+				"current_rps":          float64(stat.currentRps),
+				"current_fail_per_sec": float64(stat.currentFailPerSec),
+			},
+			eventTime)
+		o.influx.WritePoint(point)
 	}
 }
